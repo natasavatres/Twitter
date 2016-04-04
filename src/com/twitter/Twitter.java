b3de1bef248fbf258dee7c193643f2b85bcffa41
natasavatres <natasavatres@gmail.com>
2016-04-04 21:24:38 +0200

    Napisana je dokumentacija za klasu Twitter

--- src/com/twitter/Twitter.java
+++ src/com/twitter/Twitter.java
@@ -5,12 +5,21 @@ import com.twitter.poruke.TwitterPoruka;
 
 public class Twitter {
 
+	/** privatni atribut LinkedLista poruka na Twitteru 
+	 * */
 	private LinkedList<TwitterPoruka> poruke = new LinkedList<TwitterPoruka>();
 
+	/** public metoda vratiSvePoruke()
+	 * @return metoda vraca listu svih poruka na Twitteru
+	 * */
 	public LinkedList<TwitterPoruka> vratiSvePoruke() {
 		return poruke;
 	}
 
+	/** public metoda unesi() 
+	 * @param korisnik ime korisnika
+	 * @param poruka tekst koji korisnik objavljuje na Twitteru
+	 *  */
 	public void unesi(String korisnik, String poruka) {
 
 		TwitterPoruka tp = new TwitterPoruka();
@@ -20,6 +29,12 @@ public class Twitter {
 		poruke.addLast(tp);
 	}
 
+	/** public metoda vratiPoruke()
+	 * @param maxBroj najveci broj poruka sa odredjenim tagom
+	 * @param tag trazeni tag
+	 * @return niz poruka sa odredjenim tagom
+	 * @throws RuntimeException metoda baca exception
+	 * */
 	public TwitterPoruka[] vratiPoruke(int maxBroj, String tag) {
 		if (tag == null || tag == "")
 			throw new RuntimeException("Morate uneti tag");
